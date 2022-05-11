ded1b7d9296400b8bf330eb755a33bad72ec275e
Alejandro Hernandez <alejandrohernandez@MacBook-Pro-de-Macbook.local>
2022-05-11 12:03:59 -0500

    Sign up and Login

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const {
 } = require('./config/config')
 
 const postRouter = require('./routes/postRoutes')
+const userRouter = require('./routes/userRoutes')
 
 const app = express()
 
@@ -37,6 +38,7 @@ app.get('/', (req, res) => {
 
 // localhost:3000/api/v1/posts/
 app.use('/api/v1/posts', postRouter)
+app.use('/api/v1/users', userRouter)
 const port = process.env.PORT || 3000
 
 app.listen(port, () => console.log(`listening on port ${port}`))
